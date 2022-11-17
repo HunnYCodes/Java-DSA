a6235a8b43331a3646be6517fdfef3a266333686
SuNnY BiByAn <sunnykumar6121997@gmail.com>
2022-11-17 13:04:41 +0530

    for decNum to bin conversion

--- conversion.java
+++ conversion.java
@@ -15,7 +15,21 @@ public class conversion{
 
 System.out.println("decimal of " + myNum + "=" + decNum);
     }
+
+    public static void decToBin(int decNum){
+        int myNum = decNum;
+       int  pow = 0;
+       int bin  = 0;
+       while(decNum>0){
+        int rem = decNum%2;
+        bin = bin + rem * (int)Math.pow(10, pow);
+        pow++;
+        decNum = decNum/2;
+       }
+       System.out.println("Binary Number of " +myNum+ "="+bin);
+    }
     public static void main (String[] args){
         binToDec(101);
+        decToBin(5);
     }
 }

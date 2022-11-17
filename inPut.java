df65ed0258741d2164b2ded1e4816f9a3dacabeb
SuNnY BiByAn <sunnykumar6121997@gmail.com>
2022-11-17 10:37:38 +0530

    first commit

---
+++ inPut.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class inPut {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+
+        // String input1 = sc.nextLine();
+        // System.out.println(input1);
+
+        int input2 = sc.nextInt();
+        System.out.println(input2);
+    }
+}

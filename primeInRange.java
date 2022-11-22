ca1f395964ba5511682a8da8258062b420655602
SuNnY BiByAn <sunnykumar6121997@gmail.com>
2022-11-22 11:36:32 +0530

    For magicTable.java

--- primeInRange.java
+++ primeInRange.java
@@ -2,7 +2,6 @@ public class primeInRange {
     public static boolean isPrime(int n){
         if (n==2){
             return true;
-
         }
         for (int i =2; i<=Math.sqrt(n); i++){
             if(n%i==0){

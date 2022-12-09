b8783c479637a84c11db39abafeb5242eb461f8f
Nazish Khan <nazishkhan@192.168.1.58>
2022-12-09 12:52:33 +0530

    for arrays

---
+++ arrays.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class arrays{
+    public static void main(String[] args){
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+        
+        marks[0] = sc.nextByte();
+        marks[1] = sc.nextByte();
+        marks[2] = sc.nextByte();
+
+        
+        System.out.println("phy:" + marks[0]);
+        System.out.println("chem:" + marks[1]);
+        System.out.println("math:" + marks[2]);
+        
+    }
+}
